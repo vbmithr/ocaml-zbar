bd4ee7d01eb1fe90aae75d331783df873caa003d
Vincent Bernardoff <vb@luminar.eu.org>
2014-05-11 12:57:27 +0200

    Add video capture size option to the test/benchmark

--- test/test.ml
+++ test/test.ml
@@ -8,7 +8,7 @@ let rec display_speed_info nb_read ival =
   Printf.printf "%d QR/s\n%!" !nb_read;
   display_speed_info nb_read ival
 
-let main dev cached verb bench =
+let main dev dim cached verb bench =
   let nb_read = ref 0 in
   if bench then Lwt.async (fun () -> display_speed_info nb_read 1.);
   set_verbosity verb;
@@ -17,6 +17,9 @@ let main dev cached verb bench =
   ImageScanner.set_config scanner `Qrcode `Enable 1;
   ImageScanner.enable_cache scanner cached;
   let dev = Video.opendev ~dev () in
+  (match dim with
+  | Some (x, y) -> Video.request_size dev x y
+  | _ -> ());
   let imgs = Video.stream dev in
   let imgs = Lwt_stream.map (fun img -> Image.convert img "GREY") imgs in
   let symbols = Lwt_stream.map (fun img -> ImageScanner.scan_image scanner img) imgs in
@@ -35,16 +38,20 @@ let main dev cached verb bench =
 let _ =
   let open Arg in
   let device = ref "/dev/video0" in
+  let dim = ref None in
   let verbosity = ref 0 in
   let cache_enabled = ref false in
   let bench = ref false in
   let speclist = align [
+      "--dim", String
+        (fun d -> let set_dim x y = dim := Some (x, y) in Scanf.sscanf d "%dx%d" set_dim),
+      "<int>x<int> Specify video capture size, e.g. 640x480";
+      "--enable-cache", Set cache_enabled, " Enable QR-code cache to suppress doublons";
       "--bench", Set bench, " Enable benchmark mode";
       "--verbose", Set_int verbosity, "<int> Verbosity level (default: 0)";
       "-v", Set_int verbosity, "<int> Verbosity level (default: 0)";
-      "--enable-cache", Set cache_enabled, " Enable QR-code cache to suppress doublons";
     ] in
   let usage_msg = "Usage: " ^ Sys.argv.(0) ^ " [options] [/dev/video?]\nOptions are:" in
   let set_device d = device := d in
   parse speclist set_device usage_msg;
-  Lwt_main.run (main !device !cache_enabled !verbosity !bench)
+  Lwt_main.run (main !device !dim !cache_enabled !verbosity !bench)
